9c1543f63751d31e238e7b8a64b6277c37f3689e
Tim <j.tim.nolan@gmail.com>
2021-09-03 19:13:13 -0700

    Add authentication

--- multiplayer.js
+++ multiplayer.js
@@ -1,113 +1,256 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDxf3c2QdHy_e2FpdVt_AUNXA58QbqD9hI",
-    authDomain: "twine-test-1.firebaseapp.com",
-    databaseURL: "https://twine-test-1-default-rtdb.firebaseio.com",
-    projectId: "twine-test-1",
-    storageBucket: "twine-test-1.appspot.com",
-    messagingSenderId: "265490233768",
-    appId: "1:265490233768:web:15e19d3e84beba0a8dc59d"
-  };
-  window.Multiplayer = (function(){
-      var lsLockID, firebaseApp, db,
-              worldRef, roomsRef, roomRef, playerRef,
-              playerID = 'qo97yLPkmusdLMhuDX0q',
-              roomID = 'tK6r0ADXJCZzoPlmfsPe',
-              rooms = [],
-              ready = false;
-      
-      function save(){
-          if(!ready){return false};
-          const saveData = {...SugarCube.State.variables};
-          try {
-              worldRef.set(saveData.world);
-          } catch(e){console.warn(e)}
-          try {
-              roomRef.set(saveData.room);
-          } catch(e){console.warn(e)}
-          delete saveData.world;
-          delete saveData.room;
-          try {
-              playerRef.set(saveData);
-          } catch(e){console.warn(e)}
-      }
-      
-      function initDB(){
-          db = firebase.firestore();
-          getRefs();
-          buildRefEvents();
-          return Promise.all([
-              worldRef.get(),
-              roomRef.get(),
-              playerRef.get(),
-          ]);
-      }
-      
-      function getRefs(){
-          worldRef = db.collection('worldState').doc("0");
-          roomsRef = db.collection('rooms');
-          roomRef = roomsRef.doc(roomID);
-          playerRef = db.collection('players').doc(playerID);
-      }
-      
-      function buildRefEvents(){
-          worldRef.onSnapshot(doc => {
-            SugarCube.State.variables.world = doc.data();
-            console.log("World Updated: ", SugarCube.State.variables.world);
-          });
-          roomRef.onSnapshot(doc => {
-            SugarCube.State.variables.room = doc.data();
-              console.log("Room Updated: ", SugarCube.State.variables.room);
-          });
-          playerRef.onSnapshot(doc => {
-              Object.entries(doc.data()).forEach(([key, val]) => {
+window.Multiplayer = (function(){
+    var storyID, lsLockID, firebaseApp, db,
+        storyRef, roomsRef, roomRef, playersRef, playerRef,
+        storyData, roomData, playerData,
+        imports, LoadScreen,
+        playerID,
+        unsubs = {},
+        roomID = 'lobby',
+        rooms = [],
+        debug = false,
+        ready = false;
+
+    function log(){
+        if(!debug){return}
+        console.log('[MULTI] ', ...arguments);
+    }
+
+    function getUser(){
+        return firebase.auth().currentUser;
+    }
+
+    function isLoggedIn(){
+        return firebase.auth().currentUser && !firebase.auth().currentUser.isAnonymous;
+    }
+    
+    async function initAuth(){
+        function getAuthState(){
+            return new Promise(resolve => {
+                const unsub = firebase.auth().onAuthStateChanged(user => {
+                    unsub();
+                    resolve(user);
+                });
+            });
+        }
+        var user = await getAuthState();
+        if(user){
+            log("User data loaded", user.uid, user.isAnonymous ? 'anon' : 'logged in');
+        } else {
+            await initAnon();
+        }
+    }
+
+    async function initAnon(){
+        await firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
+        const data = await firebase.auth().signInAnonymously();
+        log("Anon user initialized", data.user);
+        await resetPlayerRefs();
+    }
+
+    async function login(email, password){
+        if(typeof Multiplayer.validateLogin == 'function'){
+            const errors = Multiplayer.validateLogin(email, password);
+            if(errors && errors.length > 0){
+                return {success: false, errors};
+            }
+        }
+        let response;
+        try {
+            response = await firebase.auth().signInWithEmailAndPassword(email, password);
+        } catch(e){
+            if(e.code == 'auth/user-not-found'){
+                try {
+                    const credential = firebase.auth.EmailAuthProvider.credential(email, password);
+                    response = await firebase.auth.currentUser.linkWithCredential(credential);
+                } catch(errors){
+                    return {success: false, errors};
+                }
+            } else {
+                log(response);
+                return {success: false, errors};
+            }
+        }
+        log(response);
+        await resetPlayerRefs();
+        return {success: true, response, user: response.user};
+    }
+
+    async function logout(passageName){
+        await firebase.auth().signOut();
+        await initAnon();
+        if(passageName){
+            SugarCube.Engine.play(passageName);
+        }
+        return;
+    }
+    
+    function save(){
+        if(!ready){return false};
+        const saveData = {...SugarCube.State.variables};
+        log("Saving data", playerID, saveData)
+        try {
+            storyRef.set({...storyData, state: saveData.world});
+        } catch(e){console.warn(e)}
+        try {
+            roomRef.set({...roomData, state: saveData.room});
+        } catch(e){console.warn(e)}
+        delete saveData.world;
+        delete saveData.room;
+        try {
+            playerRef.set({...playerData, state: saveData});
+        } catch(e){console.warn(e)}
+    }
+    
+    async function initDB(){
+        db = firebase.firestore();
+        await setupRefs();
+        return Promise.all([
+            storyRef.get(),
+            roomRef.get(),
+            playerRef.get(),
+        ]);
+    }
+
+    async function resetPlayerRefs(){
+        log("Starting player ref reset", unsubs);
+        const removed = ['player', 'room'].map((key) => {
+            if(typeof unsubs[key] == 'function'){
+                unsubs[key]();
+                delete unsubs[key];
+                return true;
+            }
+            return false;
+        });
+        if(removed.filter(x => x).length){
+            log("Refs reset: ", removed);
+            await setupPlayerRefs(true);
+            return Promise.all([
+                roomRef.get(),
+                playerRef.get(),
+            ]);
+        }
+    }
+
+    async function setupPlayerRefs(isReset = false){
+        const res = await playersRef.where('owner', "==", getUser().uid).get();
+        if(res.docs.length){
+            playerData = res.docs[0].data();
+            log("Player data loaded", res.docs[0].id, playerData);
+            playerID = res.docs[0].id;
+            playerRef = playersRef.doc(playerID);
+        } else {
+            playerRef = await playersRef.add({
+                owner: getUser().uid,
+                state: {},
+            });
+            log("Player created: ", playerRef.id);
+            playerID = playerRef.id;
+            Object.keys(SugarCube.State.variables).forEach(key => {
+                if(['room', 'world'].includes(key)){
+                    return;
+                }
+                delete SugarCube.State.variables[key];
+            });
+            log("Local player state reset: ", SugarCube.State.variables);
+        }
+        unsubs.player = playerRef.onSnapshot(doc => {
+            playerData = doc.data();
+            Object.entries(playerData.state).forEach(([key, val]) => {
                 SugarCube.State.variables[key] = val;
-              });
-              console.log("Player Updated: ", SugarCube.State.variables);
-          });
-      }
-      
-      async function start(){
-          lsLockID = LoadScreen.lock();
-          
-          SugarCube.State.reset();
-          SugarCube.State.variables.world = {};
-          SugarCube.State.variables.room = {};
-          
-          await initDB();
-          
-          SugarCube.Engine.play(SugarCube.Config.passages.start);
-          ready = true;
-          LoadScreen.unlock(lsLockID);
-          
-          $(document).on(':passagerender', e => {
-              save();
-          });
-      }
-      
-      function configure(){
-          SugarCube.Config.saves.autoload = false;
-          SugarCube.Config.saves.autosave = false;
-          SugarCube.Config.saves.isAllowed = () => {false};
-          SugarCube.Config.history.maxStates = 1;
-          SugarCube.Config.history.controls = false;
-          document.getElementById('menu-item-saves').style.display = 'none';
-          document.getElementById('menu-item-restart').style.display = 'none';
-      }
-      
-      function init(fc){
-          configure();
-          importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase.js")
-          .then(() => {
-              firebaseApp = firebase.initializeApp(fc);
-              start();
-          });
-      }
-      
-      return {
-          playerID,
-          roomID,
-          ready,
-          init,
-      };
-  })();
-  Multiplayer.init(firebaseConfig);
+            });
+            log("Player Updated: ", playerData.state);
+        });
+        roomRef = roomsRef.doc(roomID);
+        unsubs.room = roomRef.onSnapshot(doc => {
+            roomData = doc.data();
+            SugarCube.State.variables.room = roomData.state;
+            log("Room Updated: ", SugarCube.State.variables.room);
+        });
+    }
+    
+    async function setupRefs(){
+        storyRef = await db.collection('stories').doc(storyID);
+        roomsRef = await storyRef.collection('rooms');
+        playersRef = await storyRef.collection('players');
+        unsubs.story = storyRef.onSnapshot(doc => {
+            storyData = doc.data();
+            SugarCube.State.variables.world = storyData.state;
+            log("World Updated: ", SugarCube.State.variables.world);
+        });
+        await setupPlayerRefs();
+    }
+    
+    async function start(){
+        
+        SugarCube.State.reset();
+        SugarCube.State.variables.world = {};
+        SugarCube.State.variables.room = {};
+        
+        await initAuth();
+        await initDB();
+        
+        log("Multiplayer setup done, starting story");
+        SugarCube.Engine.play(imports.start || SugarCube.Config.passages.start);
+        ready = true;
+        LoadScreen.unlock(lsLockID);
+        
+        $(document).on(':passagerender', e => {
+            save();
+        });
+        $(document).on('submit', 'form.multiplayer-login-form', async e => {
+            e.preventDefault();
+            const formData = new FormData(e.target);
+            const res = await login(formData.get('email'), formData.get('password'));
+            const next = e.target.getAttribute('action');
+            if(next){
+                SugarCube.Engine.play(next);
+            }
+        });
+    }
+    
+    function configure(){
+        SugarCube.Config.saves.autoload = false;
+        SugarCube.Config.saves.autosave = false;
+        SugarCube.Config.saves.isAllowed = () => {false};
+        SugarCube.Config.history.maxStates = 1;
+        SugarCube.Config.history.controls = false;
+        document.getElementById('menu-item-saves').style.display = 'none';
+        document.getElementById('menu-item-restart').style.display = 'none';
+    }
+    
+    function init(id, addl){
+        storyID = id;
+        imports = addl;
+        LoadScreen = imports.LoadScreen;
+        lsLockID = imports.lockID || LoadScreen.lock();
+        SugarCube.setup.Multiplayer = this;
+        debug = addl.debug || false;
+        configure();
+        imports.importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase.js")
+        .then(() => {
+            firebaseApp = firebase.initializeApp({
+                apiKey: "AIzaSyDxf3c2QdHy_e2FpdVt_AUNXA58QbqD9hI",
+                authDomain: "twine-test-1.firebase.com",
+                databaseURL: "https://twine-test-1-default-rtdb.firebaseio.com",
+                projectId: "twine-test-1",
+                storageBucket: "twine-test-1.appspot.com",
+                messagingSenderId: "265490233768",
+                appId: "1:265490233768:web:15e19d3e84beba0a8dc59d"
+            });
+            start();
+        });
+    }
+    
+    return {
+        playerID,
+        roomID,
+        ready,
+        debug,
+        getUser,
+        init,
+        login,
+        logout,
+        isLoggedIn,
+        validateLogin: () => {},
+    };
+})();
